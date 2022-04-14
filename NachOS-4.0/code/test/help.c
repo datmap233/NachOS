67aa0a3e1c71b54cf0a10a08e79f01ef22d19e3c
datmap233 <dat030602@gmail.com>
2022-04-14 22:24:23 +0700

    NachOS

---
+++ NachOS-4.0/code/test/help.c
@@ -0,0 +1,10 @@
+#include "syscall.h"
+
+int main()
+{
+    PrintString("- Nhom gom 3 thanh vien\n- Nguyen Van Dat - 20127132\n- Nguyen Tan Hieu - 20127159\n- Tran Nhat Tan - 20127322\n");
+    PrintString("- Ascii: Chuong trinh se in ra nhung ky tu in duoc trong bang ma ascii\n");
+    PrintString("- Sort: Ban dau nhap chieu dai mang, roi nhap phan tu trong mang, nhap chieu dai (0 - tang, 1 - giam)\n");
+    Halt();
+    return 0;
+}
